1532abfe53cb3b03e0e624958c132735117427b0
Josh Marinacci <joshua@marinacci.org>
2019-01-08 10:51:00 -0800

    implement INSERT_ELEMENT handler add timestamp to all ops

--- sync.js
+++ sync.js
@@ -114,8 +114,58 @@ class ObjectSyncProtocol {
             return arr._id
         }
 
+        if(op.type === EVENT_TYPES.INSERT_ELEMENT) {
+            console.log("working on insert element")
+            const arr = this.getObjectById(op.array)
+            if (!arr) return console.error(`Cannot insert element into ${op.array} that does not exist`);
+            //check if already in there. id p entryid and prev is the same
+            if(arr._elements.some(e=>e._id === op.entryid && e._prev === op.prev)) {
+                console.log("already processed this insertion. don't fire or change")
+                return
+            }
+            console.log("inserting",op.value,'after',op.prev,'into',arr,'with id',op.entryid)
 
-            console.log(`CANNOT process operation of type ${op.type}`)
+            const elem = {
+                _id:op.entryid,
+                _value:op.value,
+                _prev:op.prev,
+                _timestamp:op.timestamp,
+                _tombstone:false
+            }
+            //calculate the index of the prev
+            const index = arr._elements.findIndex(e => e._id === op.prev)
+            console.log("index of prev is",index)
+
+            const curr = arr._elements[index+1]
+            //two forms of insert
+            if(curr && curr._prev === elem._prev) {
+                console.log(this.id, "must decide", elem._id, elem._timestamp, curr._id, curr._timestamp)
+                if(elem._timestamp > curr._timestamp) {
+                    console.log('new elem first')
+                    arr._elements.splice(index+1,0,elem)
+                } else if(elem._timestamp < curr._timestamp) {
+                    console.log("new elem second")
+                    arr._elements.splice(index+2,0,elem)
+                } else {
+                    console.log("same time. go with earliest id")
+                    if(elem._id > curr._id) {
+                        console.log('first')
+                        arr._elements.splice(index+1,0,elem)
+                    } else {
+                        console.log("second")
+                        arr._elements.splice(index+2,0,elem)
+                    }
+                }
+            } else {
+                arr._elements.splice(index+1,0,elem)
+            }
+            console.log("final array is",arr)
+            this.fire(op)
+            return
+        }
+
+
+        console.log(`CANNOT process operation of type ${op.type}`)
     }
     /*
     createObject(objid) {
@@ -340,14 +390,14 @@ class ObjectSyncProtocol {
 
     getPropertiesForObject(objid) {
         const obj = this.getObjectById(objid)
-        if(!obj) return console.error("cannot get properties for object that does not exist not exist")
+        if(!obj) return console.error(`cannot get properties for object ${objid} that does not exist not exist`)
         return Object.keys(obj)
             .filter(key => key !== '_id')
             .filter(key => key !== '_type')
     }
     getPropertyValue(objid, key) {
         const obj = this.getObjectById(objid)
-        if(!obj) return console.error("cannot get properties for object that does not exist not exist")
+        if(!obj) return console.error(`cannot get property value for object ${objid} that does not exist not exist`)
         return obj[key]
     }
     hasPropertyValue(objid,key) {
@@ -427,8 +477,9 @@ class DocGraph {
     createObject() {
         const op = {
             type: EVENT_TYPES.CREATE_OBJECT,
+            host: this.graph.getHostId(),
+            timestamp:Date.now(),
             id: this.graph.makeGUID(),
-            host: this.graph.getHostId()
         }
         return this.graph.process(op)
     }
@@ -437,6 +488,7 @@ class DocGraph {
         const op = {
             type: EVENT_TYPES.CREATE_PROPERTY,
             host: this.graph.getHostId(),
+            timestamp:Date.now(),
             object: id,
             name: name,
             value: value
@@ -448,6 +500,7 @@ class DocGraph {
         const op = {
             type: EVENT_TYPES.SET_PROPERTY,
             host: this.graph.getHostId(),
+            timestamp:Date.now(),
             object: id,
             name: name,
             value: value
@@ -478,13 +531,26 @@ class DocGraph {
         const op = {
             type: EVENT_TYPES.CREATE_ARRAY,
             host: this.graph.getHostId(),
+            timestamp:Date.now(),
             id: this.graph.makeGUID(),
         }
         return this.graph.process(op)
     }
 
     insertElement(arrid, index, elementid) {
-        return this.graph.insertElement(arrid,index,elementid)
+        const op = {
+            type: EVENT_TYPES.INSERT_ELEMENT,
+            host: this.graph.getHostId(),
+            timestamp:Date.now(),
+            array:arrid,
+            value:elementid,
+            entryid: this.graph.makeGUID(),
+            prev: -1,
+        }
+        if(index > 0) {
+            op.prev = this.graph.getElementAt(arrid,index-1)._id
+        }
+        return this.graph.process(op)
     }
 }
 
@@ -493,7 +559,7 @@ class HistoryView {
         this.history = []
         this.listeners = []
         graph.onChange((e)=>{
-            const obj = { type:e.type}
+            const obj = { type:e.type, host: e.host, timestamp: e.timestamp}
             if(e.type === EVENT_TYPES.CREATE_PROPERTY || e.type === EVENT_TYPES.SET_PROPERTY) {
                 obj.name = e.name
                 obj.value = e.value
